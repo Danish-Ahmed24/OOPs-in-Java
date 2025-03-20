b2042bed29928c767cf48e07e000266198cb5b7d
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-20 14:49:19 +0500

    Car and Engine attr

--- lab6/Q1.java
+++ lab6/Q1.java
@@ -5,5 +5,15 @@ public class Q1 {
 
     }
 }
+class Car{
+    private String brandName;
+    private String model;
+    private int chassisNumber;
+    private Engine engine;
+}
 
-
+class Engine{
+    private int horsePower;
+    private String fuelType;
+    private int capacity;
+}
