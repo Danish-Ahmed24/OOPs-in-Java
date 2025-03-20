42c9f63654189f6a7f1c9e46f06cec5c5bb6bf4d
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-20 14:43:29 +0500

    Lab_6_Q1: car class created

--- lab6/src/Question1/Main.java
+++
@@ -1,9 +0,0 @@
-package Question1;
-
-public class Main
-{
-    public static void main(String[] args) {
-        JavaCourse j = new JavaCourse("OOPs lab","123","A3",3,"Sir SAad");
-        j.display();
-    }
-}
