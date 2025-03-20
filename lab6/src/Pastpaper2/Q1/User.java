3e380cc03ef9d662a10eec199cef08a2f8d52305
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-20 14:39:10 +0500

    start of lab 6

--- lab6/src/Pastpaper2/Q1/User.java
+++
@@ -1,6 +0,0 @@
-package Pastpaper2.Q1;
-
-public class User {
-    private int noOfCopies;
-    private int bill;
-}
--- lab6/src/Question4/Management.java
+++
@@ -1,9 +0,0 @@
-//package Question4;
-//
-//public class Management {
-//    private int noStudents;
-//    private int noWeek;
-//    private int [][]attendance = new int[noStudents][noWeek];
-//
-//    void input
-//}
