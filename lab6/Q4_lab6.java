24d0b0f22f8a064a216510c8738da8dc2509e6be
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-20 20:24:24 +0500

    Q4_lab6 will continue soon...

--- lab6/Q4_lab6.java
+++ lab6/Q4_lab6.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 //attendance records for students over several weeks. The system should store attendance data where each row
 //represents a student and each column represents a week. For example, if there are M students and W weeks, the
 //array will be of size M x W. The system should allow the user to input the attendance for each student in each week
-//        (with 1 for present and 0 for absent). After storing the data, the system should calculate and display the total
+//        (with 1 for present and 0 for absent). After storing the data,$$$$ the system should calculate and display the total
 //attendance for each student, the weekly attendance across all students, and the average class attendance.
 //Additionally, the system should identify students with the best and worst attendance records. This task illustrates
 //the use of a two-dimensional array for managing attendance records and performing calculations across multiple
@@ -17,6 +17,7 @@ public class Q4_lab6 {
 class Attendance {
     Scanner in = new Scanner(System.in);
     int M,W,temp;
+    boolean check;
     int [][]attendance;
     Attendance(int M,int W)
     {
@@ -24,13 +25,23 @@ class Attendance {
     }
     public void markAttendance()
     {
-        for(int[] M : attendance)
-        {
-            for(int W : M)
+        for(int i =0 ; i<M ;i++)
+        {   check = true;
+            for(int j =0; j<W ;j++)
             {
-                System.out.println("Mark attendance[1/0]: ");
-                temp=in.nextInt();
+                check = true;
+                while(check)
+                {
 
+                System.out.println("Mark attendance[1/0] ["+i+"]["+j+"] : ");
+                temp=in.nextInt();
+                if(temp==1 || temp ==0) {
+                    attendance[i][j]=temp;
+                    check = false;
+                } else {
+                    System.out.print("ERROR: ");
+                }
+                }
             }
         }
     }
