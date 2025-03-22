1dc15af4e3bd759a6a2d41a715bab121b4c46608
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-23 02:54:22 +0500

    Finishing of Q4

--- lab6/Q4_lab6.java
+++ lab6/Q4_lab6.java
@@ -2,61 +2,105 @@ package lab6;
 
 import java.util.Scanner;
 
-//You are tasked with creating a Student Attendance Management System using a two-dimensional array to track
-//attendance records for students over several weeks. The system should store attendance data where each row
-//represents a student and each column represents a week. For example, if there are M students and W weeks, the
-//array will be of size M x W. The system should allow the user to input the attendance for each student in each week
-//(with 1 for present and 0 for absent). After storing the data,$$$$ the system should calculate and display
-//the total attendance for each student, the weekly attendance across all students, and the average class attendance.
-//Additionally, the system should identify students with the best and worst attendance records. This task illustrates
-//the use of a two-dimensional array for managing attendance records and performing calculations across multiple
-//dimensions of data.
 public class Q4_lab6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter M : ");
+        System.out.println("Enter M (Number of Students): ");
         int M = in.nextInt();
-        System.out.println("Enter W : ");
+        System.out.println("Enter W (Number of Weeks): ");
         int W = in.nextInt();
-        Attendance attendance = new Attendance(M,W);
+        Attendance attendance = new Attendance(M, W);
         attendance.markAttendance();
+        attendance.displayAttendanceSummary();
     }
 }
+
 class Attendance {
     Scanner in = new Scanner(System.in);
-    int M,W,temp;
-    boolean check;
-    int [][]attendance;
-    Attendance(int M,int W)
-    {
-        this.M=M;
-        this.W=W;
+    int M, W;
+    int[][] attendance;
+
+    Attendance(int M, int W) {
+        this.M = M;
+        this.W = W;
         attendance = new int[M][W];
     }
-    public void markAttendance()
-    {
-        for(int i =0 ; i<M ;i++)
-        {
-            check = true;
-            for(int j =0; j<W ;j++)
-            {
-                check = true;
-                while(check)
-                {
-                System.out.println("Mark attendance[1/0] ["+i+"]["+j+"] : ");
-                temp=in.nextInt();
-                if(temp==1 || temp ==0) {
-                    attendance[i][j]=temp;
-                    check = false;
-                } else {
-                    System.out.print("ERROR: ");
-                }
+
+    public void markAttendance() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < W; j++) {
+                boolean validInput = false;
+                while (!validInput) {
+                    System.out.println("Mark attendance [1/0] for Student " + (i + 1) + " Week " + (j + 1) + ": ");
+                    int temp = in.nextInt();
+                    if (temp == 1 || temp == 0) {
+                        attendance[i][j] = temp;
+                        validInput = true;
+                    } else {
+                        System.out.println("ERROR: Please enter 1 (Present) or 0 (Absent). Try again.");
+                    }
                 }
             }
         }
     }
-    public int calcTotalAttEachStd()
-    {
-        int tot =
+
+    public int calcTotalAttEachStd(int studentIndex) {
+        int total = 0;
+        for (int j = 0; j < W; j++) {
+            total += attendance[studentIndex][j];
+        }
+        return total;
+    }
+
+    public int[] calcWeeklyAttendance() {
+        int[] weeklyTotal = new int[W];
+        for (int j = 0; j < W; j++) {
+            for (int i = 0; i < M; i++) {
+                weeklyTotal[j] += attendance[i][j];
+            }
+        }
+        return weeklyTotal;
+    }
+
+    public double calcAverageAttendance() {
+        int totalAttendance = 0;
+        for (int i = 0; i < M; i++) {
+            totalAttendance += calcTotalAttEachStd(i);
+        }
+        return (double) totalAttendance / (M * W) * 100;
+    }
+
+    public void displayAttendanceSummary() {
+        System.out.println("\n--- Attendance Summary ---");
+
+        // Total attendance for each student
+        int bestStudent = 0, worstStudent = 0;
+        int maxAttendance = 0, minAttendance = W;
+        for (int i = 0; i < M; i++) {
+            int total = calcTotalAttEachStd(i);
+            System.out.println("Total attendance for Student " + (i + 1) + ": " + total);
+
+            if (total > maxAttendance) {
+                maxAttendance = total;
+                bestStudent = i;
+            }
+            if (total < minAttendance) {
+                minAttendance = total;
+                worstStudent = i;
+            }
+        }
+
+        // Weekly attendance
+        int[] weeklyAttendance = calcWeeklyAttendance();
+        for (int j = 0; j < W; j++) {
+            System.out.println("Total attendance for Week " + (j + 1) + ": " + weeklyAttendance[j]);
+        }
+
+        // Average attendance
+        System.out.printf("Average class attendance: %.2f%%\n", calcAverageAttendance());
+
+        // Best and Worst attendance
+        System.out.println("Student with Best Attendance: Student " + (bestStudent + 1) + " (" + maxAttendance + "/" + W + ")");
+        System.out.println("Student with Worst Attendance: Student " + (worstStudent + 1) + " (" + minAttendance + "/" + W + ")");
     }
 }
