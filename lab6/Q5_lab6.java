4228d724794b1bb7d33e81f7730eb3ed1e84b9d4
Danish-Ahmed24 <danishsp21@gmail.com>
2025-03-23 03:48:05 +0500

    Q5 lab6 done!

--- lab6/Q5_lab6.java
+++ lab6/Q5_lab6.java
@@ -7,21 +7,45 @@ public class Q5_lab6 {
     static ArrayList<Employee> employees = new ArrayList<>();
     public static void main(String[] args) {
         Manage manager = new Manage();
+        Scanner in = new Scanner(System.in);
+        boolean exit = true;
 
-        while(true)
-        {
-            if(manager.displayMenu() == '1' || manager.displayMenu()=='2'||
-                    manager.displayMenu() == '3' || manager.displayMenu()=='4'||
-                    manager.displayMenu() == '5'
-            )
+
+            while(exit)
             {
-                break;
+                char c = manager.displayMenu();
+                if(c == '1' || c=='2'||
+                        c == '3' || c=='4'||
+                        c == '5'
+                )
+                {
+                    switch (c)
+                    {
+                        case '1':
+                            manager.add();
+                            break;
+                        case '2':
+                            manager.displayAll();
+                            break;
+                        case '3':
+                            String dp;
+                            System.out.println("Enter department: ");
+                            dp=in.next();
+                            manager.search(dp);
+                        case '4':
+                            System.out.println("Average salary is "+manager.avgSalary());
+                        case '5':
+                            exit = false;
+                            break;
+
+                    }
+                }
+                else
+                {
+                    System.out.println("Enter valid option (1 to 5)");
+                }
             }
-            else
-            {
-                System.out.println("Enter valid option (1 to 5)");
-            }
-        }
+
 
     }
 }
@@ -31,7 +55,23 @@ class Employee {
     private String department;
     private long salary;
 
-    public Employee(int id,String name,String department,long salary)
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+
+    public Employee(int id, String name, String department, long salary)
     {
         this.employeeId=id;
         this.employeeName=name;
@@ -53,5 +93,48 @@ class Manage{
         System.out.println("Enter the choice: ");
         ch = in.next().charAt(0);
         return ch;
+    }
+    public long avgSalary()
+    {
+        long avg =0;
+        for(Employee e : Q5_lab6.employees)
+        {
+            avg+=e.getSalary();
+        }
+        avg /=Q5_lab6.employees.size();
+        return (avg);
+    }
+    public void add()
+    {int id;
+        String name;
+        String department;
+        long salary;
+        System.out.println("Enter ID: ");
+        id=in.nextInt();
+        System.out.println("Enter name: ");
+        name=in.next();
+        System.out.println("Enter department: ");
+        department=in.next();
+        System.out.println("Enter salary: ");
+        salary=in.nextLong();
+        Employee e = new Employee(id,name,department,salary);
+        System.out.println("Employee Added!");}
+    public void displayAll()
+    {
+        for(Employee e : Q5_lab6.employees)
+        {
+            System.out.println("ID: "+e.getEmployeeId()+" Name: "+e.getEmployeeName()+"\n Department: "+e.getDepartment()+ " Salary: "+e.getSalary());
+        }
+    }
+    public void search(String department)
+    {
+                System.out.print("IDs: ");
+        for(Employee e : Q5_lab6.employees)
+        {
+            if(e.getDepartment().equals(department))
+            {
+                System.out.print(" "+e.getEmployeeId());
+            }
+        }
     }
 }
