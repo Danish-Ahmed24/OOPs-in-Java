3255b7f8d983328772a182e07abad842ee379155
Danish-Ahmed24 <danishsp21@gmail.com>
2025-05-02 21:09:08 +0500

    Q1 lab11 done

---
+++ lab11/Q1_lab11.java
@@ -0,0 +1,35 @@
+package lab11;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class Q1_lab11 {
+    public static void main(String[] args) {
+        ArrayList<String> elements = new ArrayList<>();
+        elements.add("apple");
+        elements.add("mango");
+        elements.add("orange");
+        elements.add("grapes");
+        elements.add("melon");
+        try {
+
+            BufferedWriter file = new BufferedWriter(new FileWriter("Q1_lab11.txt"));
+            for(String element : elements)
+            {
+                file.write(element);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        File file = new File("Q1_lab11.txt");
+        if(file.delete()){
+            System.out.println("Deleted success");
+        }else{
+            System.out.println("Cant delete");
+        }
+    }
+}
